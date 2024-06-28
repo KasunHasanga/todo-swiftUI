a3f76ae398afc31c710d10001f04bc7a36d25ad6
kasun Hasanga <Kasunhasanga96@gmail.com>
2024-06-28 10:13:48 +0530

    Initial Commit

---
+++ ToDOList/ToDOListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDOListApp.swift
+//  ToDOList
+//
+//  Created by kasun Hasanga on 2024-06-28.
+//
+
+import SwiftUI
+
+@main
+struct ToDOListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
