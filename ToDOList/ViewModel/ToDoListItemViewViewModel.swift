0ff4e1242257b99e9108fef9bc466f7e29e79180
kasun Hasanga <Kasunhasanga96@gmail.com>
2024-07-03 12:05:25 +0530

    error handling and profile view show

--- ToDOList/ViewModel/ToDoListItemViewViewModel.swift
+++ ToDOList/ViewModel/ToDoListItemViewViewModel.swift
@@ -4,7 +4,8 @@
 //
 //  Created by kasun Hasanga on 2024-06-28.
 //
-
+import Firebase
+import FirebaseFirestore
 import Foundation
 
 /// ViewModel for Single to do list item  (Each tow in item list)
@@ -12,6 +13,24 @@ import Foundation
 class ToDoListItemViewViewModel: ObservableObject {
     
     init() {
+        
+    }
+    func toggleIsDone(item: ToDoListItem){
+        
+        var itemCopy = item
+        itemCopy.setDone(!item.isDone)
+        
+        guard let uid = Auth.auth().currentUser?.uid else{
+            return
+        }
+        
+        let db = Firestore.firestore()
+        
+        db.collection("users")
+            .document(uid)
+            .collection("todos")
+            .document(itemCopy.id)
+            .setData(itemCopy.asDictionary())
         
     }
     
